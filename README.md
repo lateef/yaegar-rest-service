1cd3e113629ddcc393bdb6c0b395ba5eb704a218
Lateef Adeniji-Adele <adenijiadele@yahoo.com>
2018-12-09 21:39:43 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# yaegar-rest-service
---
+++ src/main/java/com/yaegar/yaegarrestservice/YaegarRestServiceApplication.java
@@ -0,0 +1,12 @@
+package com.yaegar.yaegarrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaegarRestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YaegarRestServiceApplication.class, args);
+    }
+}
